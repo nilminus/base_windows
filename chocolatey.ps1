643e491c022930f6880fcb9c1a36763d052fe4b6
nilminus <nilminus@gmail.com>
2016-01-23 18:27:56 +0200

    first

---
+++ chocolatey.ps1
@@ -0,0 +1,38 @@
+if ((which cinst) -eq $null) {
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    choco feature enable -n=allowGlobalConfirmation
+}
+
+# Browsing
+cinst firefox
+cinst googlechrome
+cinst firefox
+cinst opera
+cinst fiddler
+
+# Net
+cinst dropbox
+cinst evernote
+cinst skype
+
+# Development
+cinst wget
+cinst curl
+cinst wput
+cinst wincommandpaste
+cinst winmerge
+cinst vim
+cinst githubforwindows
+cinst hg
+cinst nodejs.install
+cinst nuget.commandline
+cinst putty
+cinst ruby
+cinst sublimetext2
+cinst webpi
+cinst webpicommandline
+
+# Misc
+cinst vlc
+cinst virtualclonedrive
