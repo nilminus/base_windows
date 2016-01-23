7f144425879a62c9b4340d3cedd383cd3f0e10d4
Nil Minus <nilminus@gmail.com>
2016-01-23 18:37:07 +0200

    Update chocolatey.ps1

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -4,19 +4,21 @@ if ((which cinst) -eq $null) {
     choco feature enable -n=allowGlobalConfirmation
 }
 
-# Browsing
+# Net
 cinst firefox
 cinst googlechrome
 cinst firefox
 cinst opera
 cinst fiddler
-
-# Net
 cinst dropbox
 cinst evernote
 cinst skype
+cinst teamviewer
+cinst thunderbird
 
 # Development
+cinst python2
+cinst conemu
 cinst wget
 cinst curl
 cinst wput
@@ -29,17 +31,23 @@ cinst nodejs.install
 cinst nuget.commandline
 cinst putty
 cinst ruby
-cinst sublimetext2
+cinst sublimetext3
 cinst webpi
 cinst webpicommandline
 cinst notepadplusplus
+cinst jre7
+cinst jdk7
 
 # Misc
 cinst vlc
 cinst virtualclonedrive
-cinst jre7
-cinst 7zip
-cinst autohotkey.portable
+cinst winrar
+cinst autohotkey
+cinst virtualbox
+cinst calibre
+
 
 # Security?
 cinst sysinternals
+cinst procexp
+cinst wireshark
